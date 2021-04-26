187aada98af81292ca654b36bc07f27c7507ebb9
Jere <jj.stigell@gmail.com>
2021-04-26 19:17:28 +0300

    1.3

--- osa1/kurssitiedot/App.js
+++ osa1/kurssitiedot/App.js
@@ -2,20 +2,26 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course = {course}/>
-      <Content part1 = {part1} excersises1 = {exercises1}
-      part2 = {part2} excersises2 = {exercises2}
-      part3 = {part3} excersises3 = {exercises3}/>
-      <Total ex1 = {exercises1} ex2 = {exercises2} ex3 = {exercises3}/>
+      <Content part1 = {part1.name} excersises1 = {part1.exercises}
+      part2 = {part2.name} excersises2 = {part2.exercises}
+      part3 = {part3.name} excersises3 = {part3.exercises}/>
+      <Total ex1 = {part1.exercises} ex2 = {part2.exercises} ex3 = {part3.exercises}/>
     </div>
   )
 }
