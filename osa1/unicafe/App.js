2f8bc0725ca08927a7831429aba9530c67950ae9
Jere <jj.stigell@gmail.com>
2021-05-02 22:23:37 +0300

    1.7

--- osa1/unicafe/App.js
+++ osa1/unicafe/App.js
@@ -6,13 +6,9 @@ const App = () => {
 	const [neutral, setNeutral] = useState(0)
 	const [bad, setBad] = useState(0)
 
-
-
-	//total, keskiarvo, positiivisten prosentti
-
   return (
 		<div>
-			<h1>give feed back</h1>
+			<h1>give feedback</h1>
 			<button onClick={ () => setGood(good +1)}>good</button>
 			<button onClick={ () => setNeutral(neutral + 1)}>neutral</button>
 			<button onClick={ () => setBad(bad + 1)}>bad</button>
@@ -20,30 +16,11 @@ const App = () => {
 			<p>good {good}</p>
 			<p>neutral {neutral}</p>
 			<p>bad {bad}</p>
-			
-			<Total good={good} neutral={neutral} bad={bad}/>
-			<Average good={good} neutral={neutral} bad={bad}/>
-			<PositiveFeedback good={good} neutral={neutral} bad={bad}/>
+			<p>all {good + neutral + bad}</p>
+			<p>average {(good * 1 + bad * -1) / (good + neutral + bad)}</p>
+			<p>positive {(good / (good + neutral + bad)) * 100} %</p>
 		</div>
   )
-}
-
-const Total = ({good, neutral, bad}) => {
-	return (
-	<p>all {good + neutral + bad}</p>
-	)
-}
-
-const Average = ({good, neutral, bad}) => {
-	return (
-	<p>average {(good * 1 + bad * -1) / (good + neutral + bad)}</p>
-	)
-}
-
-const PositiveFeedback = ({good, neutral, bad}) => {
-	return (
-	<p>positive {(good / (good + neutral + bad)) * 100} %</p>
-	)
 }
 
 export default App
