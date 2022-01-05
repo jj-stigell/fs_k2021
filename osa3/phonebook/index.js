9ca3139c8875ee3d81fbf86733ab94f0b6164fa0
jj-stigell <jj.stigell@gmail.com>
2022-01-05 14:43:32 +0900

    3.2

--- osa3/phonebook/index.js
+++ osa3/phonebook/index.js
@@ -8,6 +8,17 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+
+  const time = Date();
+  const amount = Object.keys(persons).length;
+  response.send(`
+    <p>Phonebook has info for ${amount} people.</p>
+    <br>
+    <p>${time}</p>
+  `)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
