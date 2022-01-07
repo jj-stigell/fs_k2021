24720ab53e4ef3ea501a4e3c91e369004d05f1c0
jj-stigell <jj.stigell@gmail.com>
2022-01-07 12:02:52 +0900

    3.22

--- osa3/phonebook/index.js
+++ osa3/phonebook/index.js
@@ -1,9 +1,9 @@
 
 require('dotenv').config()
-const { request, response } = require('express');
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
-let persons = require('./persons.json');
+
 const morgan = require('morgan')
 const CORS = require('cors')
 const Person = require('./models/person')
@@ -16,85 +16,85 @@ app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+	console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	} else if (error.name === 'ValidationError') {
+		return response.json(error)
+	}
 
-  next(error)
+	next(error)
 }
 
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+	Person.find({}).then(persons => {
+		response.json(persons)
+	})
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id)
-  .then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+	Person.findById(request.params.id)
+		.then(person => {
+			if (person) {
+				response.json(person)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-  Person.find({}).then(persons => {
-    const time = Date();
-    const amount = Object.keys(persons).length;
-    response.send(`
+	Person.find({}).then(persons => {
+		const time = Date()
+		const amount = Object.keys(persons).length
+		response.send(`
       <p>Phonebook has info for ${amount} people.</p>
       <br>
       <p>${time}</p>
     `)
-  })
+	})
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndRemove(request.params.id)
-  .then(result => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
+app.delete('/api/persons/:id', (request, response, next) => {
+	Person.findByIdAndRemove(request.params.id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 app.post('/api/persons', async (request, response, next) => {
 
-  const body = request.body
+	const body = request.body
 
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  })
+	const person = new Person({
+		name: body.name,
+		number: body.number,
+	})
 
-  person.save().then(savedPerson => {
-    Person.find({}).then(persons => {
-      response.json(persons)
-    })
-  })
-  .catch(error => next(error))
+	person.save().then(savedPerson => {
+		Person.find({}).then(persons => {
+			response.json(persons)
+		})
+	})
+		.catch(error => next(error))
 })
 
 
 app.put('/api/persons/:id', (request, response, next) => {
-  Person.findByIdAndUpdate(request.params.id, request.body, { new: true })
-    .then(updatedPerson => {
-      response.json(updatedPerson)
-    })
-    .catch(error => next(error))
+	Person.findByIdAndUpdate(request.params.id, request.body, { new: true })
+		.then(updatedPerson => {
+			response.json(updatedPerson)
+		})
+		.catch(error => next(error))
 })
 
 
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+	response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
@@ -102,5 +102,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
