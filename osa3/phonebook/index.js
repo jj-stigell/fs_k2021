e48bdd3f98e5502c49514a12dd41fc1cd4392eb6
jj-stigell <jj.stigell@gmail.com>
2022-01-07 09:34:59 +0900

    3.19

--- osa3/phonebook/index.js
+++ osa3/phonebook/index.js
@@ -20,7 +20,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
@@ -63,19 +65,9 @@ app.delete('/api/persons/:id', (request, response) => {
   .catch(error => next(error))
 })
 
-app.post('/api/persons', async (request, response) => {
+app.post('/api/persons', async (request, response, next) => {
 
   const body = request.body
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'some content missing' 
-    })
-  } else if ((persons.find(person => person.name === body.name))) {
-    return response.status(400).json({ 
-      error: 'name must be unique' 
-    })
-  }
 
   const person = new Person({
     name: body.name,
@@ -87,16 +79,19 @@ app.post('/api/persons', async (request, response) => {
       response.json(persons)
     })
   })
+  .catch(error => next(error))
 })
+
 
 app.put('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndUpdate(request.params.id, request.body, { new: true })
     .then(updatedPerson => {
-      console.log(updatedPerson)
       response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
+
+
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
