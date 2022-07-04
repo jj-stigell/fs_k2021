179222a0bde170348993c2ae14887a3c37a9ba93
Jere Stigell <jj.stigell@gmail.com>
2022-07-04 21:48:23 +0900

    4.2 Blog list refactor

--- osa4/index.js
+++ osa4/index.js
@@ -1,44 +1,9 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const mongoUrl = process.env.MONGODB_URI
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const server = http.createServer(app)
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
