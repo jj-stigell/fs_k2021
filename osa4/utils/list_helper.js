55300f2fd623ec20934bd310575a5bb6abca3b27
Jere Stigell <jj.stigell@gmail.com>
2022-07-05 00:32:01 +0900

    4.5

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -3,16 +3,25 @@ const dummy = (blogs) => {
     return 1
 }
 
-const totalLikes = (blog) => {
+const totalLikes = (blogs) => {
     const reducer = (sum, item) => {
         return sum + item.likes
     }
-    return blog.length === 0
+    return blogs.length === 0
         ? 0
-        : blog.reduce(reducer, 0)
+        : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    let mostLiked = blogs[0]
+    blogs.forEach(element => {
+        element.likes > mostLiked.likes ? mostLiked = element : 1
+    });
+    return [mostLiked]
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
