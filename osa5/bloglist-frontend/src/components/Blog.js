378e0185c8911cd6d8d7271075473f8aca7080cf
Jere Stigell <jj.stigell@gmail.com>
2022-07-12 21:23:28 +0900

    5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div key={blog._id}>
-    <b>Title:</b> {blog.title} <b>Author:</b> {blog.author} <b>Url:</b> {blog.url} <b>Id:</b> {blog._id}
-  </div>
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [detailsVisible, setVisibility] = useState(false)
+  const hideWhenVisible = { display: detailsVisible ? 'none' : '' }
+  const showWhenVisible = { display: detailsVisible ? '' : 'none' } 
+
+  return (
+    <div style={blogStyle} key={blog._id}>
+      <b>Title:</b> {blog.title} 
+      <div style={showWhenVisible}>
+        <b>Author:</b> {blog.author}
+        <b>Url:</b> {blog.url}
+        <b>Id:</b> {blog._id}
+        <b>Likes:</b> {blog.likes}
+        <button onClick={() => setVisibility(false)}>hide</button>
+        <button>like</button>
+      </div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setVisibility(true)}>view</button>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
