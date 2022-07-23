ef69eb6eed36f574f42c398addac60d4854c6659
Jere Stigell <jj.stigell@gmail.com>
2022-07-23 18:51:27 +0900

    8.4

--- osa8/index.js
+++ osa8/index.js
@@ -108,16 +108,16 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(name: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
-
+//books
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => args.name ? books.filter(book => book.author === args.name): books,
     allAuthors: () => authors,
   },
   Author: {
