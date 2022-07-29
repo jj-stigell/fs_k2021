775a0655b4c9abfc25bc29737664f2b48520f9eb
Jere Stigell <jj.stigell@gmail.com>
2022-07-29 17:02:45 +0900

    9.10

--- osa9/patientor/backend/src/index.ts
+++ osa9/patientor/backend/src/index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
-const cors = require('cors')
+import diagnoseRouter from './routes/diagnoses';
+const cors = require('cors');
 const app = express();
-app.use(express.json());
-app.use(cors())
-
 const PORT = 3001;
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ osa9/patientor/backend/src/services/diagnoseService.ts
@@ -0,0 +1,10 @@
+import diagnosesData from '../data/diagnoses.json';
+import { Diagnose } from '../types';
+
+const diagnoses: Array<Diagnose> = diagnosesData as Array<Diagnose>;
+
+const getEntries = (): Array<Diagnose> => {
+  return diagnoses;
+};
+
+export default { getEntries };
