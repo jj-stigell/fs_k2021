d46d70753283d7aecbf916270281988b76fb7a5c
Jere Stigell <jj.stigell@gmail.com>
2022-08-02 16:21:47 +0900

    9.15

--- osa9/react_ts_project/src/App.tsx
+++ osa9/react_ts_project/src/App.tsx
@@ -1,42 +1,125 @@
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the easy course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the hard course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special"
     }
-  ];
+  ]
 
   interface header {
     name: string
   }
 
-  interface courseContent {
-    name: string;
-    exerciseCount: number
+  interface content {
+    courses: Array<CoursePartBase>
   }
 
-  interface content {
-    courses: Array<courseContent>
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
   }
+
+  interface CoursePartDescription extends CoursePartBase {
+    description: string;
+  }
+
+  interface CourseNormalPart extends CoursePartDescription {
+    type: "normal";
+  }
+  interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+  }
+
+  interface CourseSubmissionPart extends CoursePartDescription {
+    type: "submission";
+    exerciseSubmissionLink: string;
+  }
+
+  interface CourseSpecialPart extends CoursePartDescription {
+    requirements: Array<string>;
+  }
+
+  type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
 
   const Header = (props: header) => {
-    return <h1>Hello, {props.name}</h1>;
+    return <h1>{props.name}</h1>;
   }
 
   const Content = (props: content) => {
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const renderData = (course: any) => {
+      switch (course.type) {
+        case "normal":
+          return (
+            <p key={course.name}>
+              <strong>{course.name} {course.exerciseCount}</strong><br/>
+              {course.description}
+            </p>
+          );
+        case "groupProject":
+          return (
+            <p key={course.name}>
+              <strong>{course.name} {course.exerciseCount}</strong><br/>
+              project exercises {course.groupProjectCount}
+            </p>
+          );
+        case "submission":
+          return (
+            <p key={course.name}>
+              <strong>{course.name} {course.exerciseCount}</strong><br/>
+              {course.description}<br/>
+              submit to {course.exerciseSubmissionLink}
+            </p>
+          );
+        case "special":
+          return (
+            <p key={course.name}>
+              <strong>{course.name} {course.exerciseCount}</strong><br/>
+              {course.description}<br/>
+              required skills: {course.requirements.join(", ")}
+            </p>
+          );
+        default:
+          return("")
+      }
+    };
+  
     return (
       <div>
         {props.courses.map((course) => (
-          <p key={course.name}>{course.name} {course.exerciseCount}</p>
+          renderData(course)
         ))}
       </div>
     )
